41e94ac3d778e408880c8e342baca2c4e90d0d36
Athos Guariza <athosguariza@gmail.com>
2025-10-24 16:29:35 +0000

    chave de acesso ao banco de dados e // Import the functions you need fro

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-7651274200-eb2ae",
-  "appId": "1:356667822231:web:b3525c38ab75a7555099d2",
-  "apiKey": "AIzaSyCih_tqoX0pzh-rp5wgmrQDOJEPgpm0GgM",
-  "authDomain": "studio-7651274200-eb2ae.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "356667822231"
+  apiKey: "AIzaSyCih_tqoX0pzh-rp5wgmrQDOJEPgpm0GgM",
+  authDomain: "studio-7651274200-eb2ae.firebaseapp.com",
+  projectId: "studio-7651274200-eb2ae",
+  storageBucket: "studio-7651274200-eb2ae.firebasestorage.app",
+  messagingSenderId: "356667822231",
+  appId: "1:356667822231:web:9bcb5647d7ed998a5099d2"
 };
