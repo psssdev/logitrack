dc254da5e7d7084d8b3bf4da47e0713630ec4b0f
Athos Guariza <athosguariza@gmail.com>
2025-10-24 16:05:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7651274200-eb2ae",
+  "appId": "1:356667822231:web:b3525c38ab75a7555099d2",
+  "apiKey": "AIzaSyCih_tqoX0pzh-rp5wgmrQDOJEPgpm0GgM",
+  "authDomain": "studio-7651274200-eb2ae.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "356667822231"
+};
